07fda4f7254a7344c54c3a965bd9b906e2a4245c
Petter Elvevoll <petterelvevoll@gmail.com>
2024-10-30 14:37:27 +0100

    Sjekker at jeg kjønner eksemplene

--- EFGetStarted/Program.cs
+++ EFGetStarted/Program.cs
@@ -28,3 +28,33 @@ db.SaveChanges();
 Console.WriteLine("Delete the blog");
 db.Remove(blog);
 db.SaveChanges();
+
+Console.WriteLine("Lager min egen blog");
+db.Add(new Blog { Url = "https://blog.petterelvevoll.no/tekblog" });
+db.SaveChanges();
+
+Console.WriteLine("Bruker Linq for å finne igjen min egen blog");
+var tekBlog = db.Blogs
+    .OrderBy(b => b.BlogId)
+    .First();
+
+Console.WriteLine("Legger ut de første blog postene");
+tekBlog.Posts.Add(
+    new Post { Title = "C# med EF", Content = "Første blog laget med Entity Foundation Core!" });
+tekBlog.Posts.Add(
+    new Post { Title = "C# + EF + Razor + ASP.Net", Content = "Nettsiden er nå live :D"});
+db.SaveChanges();
+
+var mine_blog_poster = tekBlog.Posts
+    .OrderBy(b => b.PostId);
+
+Console.WriteLine("Alle mine poster levert rett til skjermen\n");
+foreach (var post in mine_blog_poster)
+{
+    Console.WriteLine("\t" + post.Title);
+    Console.WriteLine("\t\t" + post.Content);
+}
+
+Console.WriteLine("Sletter bloggen min :(");
+db.Remove(tekBlog);
+db.SaveChanges();
